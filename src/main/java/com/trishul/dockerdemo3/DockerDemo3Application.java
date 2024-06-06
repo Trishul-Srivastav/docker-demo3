d162288012d1afb7e3d1fd1e9487a49f9e81605b
trishul srivastav <trishulsrivastav97@gmail.com>
2024-06-06 09:02:53 +0530

    intial commit

---
+++ src/main/java/com/trishul/dockerdemo3/DockerDemo3Application.java
@@ -0,0 +1,13 @@
+package com.trishul.dockerdemo3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerDemo3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerDemo3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/trishul/dockerdemo3/DockerDemo3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trishul.dockerdemo3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerDemo3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
